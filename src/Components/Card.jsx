f2bcec2079107a500f0470370bb4b69a65fc315a
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-16 14:34:05 +0300

    props

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,16 @@
 import Sports from '../assets/sports.png'
 import Star from '../assets/Star.png'
-const Card = () => {
+const Card = (props) => {
     return(
         <div className='card'>
-            <img src={Sports} className="sport--logo" alt="" />
+            <img src={props.img} className="sport--logo" alt="" />
             <div className='card--stats'>
                 <img src={Star} className="star--logo" alt="" />
-               <span className='gray'>5.0</span>
-                <span className='gray'>(6) . </span>      
+               <span className='gray'>{props.rating}</span>
+                <span className='gray'>{props.reviewCount}</span>      
             </div>
-                <p>Life lessons with Katie Zafer</p>
-                <p><span className='bolder'>From $136 </span> / person</p>
+                <p>{props.title}</p>
+                <p><span className='bolder'>From ${props.price}</span> / person</p>
         </div>
     )
 }
